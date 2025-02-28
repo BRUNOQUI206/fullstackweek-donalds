79f17a0e481a754268a9c27069894f95353e053e
Bruno <brunoguilher1@gmail.com>
2025-02-28 15:14:16 -0300

    feat: ensure order can be created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Toaster } from "sonner";
 
 import { CartProvider } from "./[slug]/menu/context/cart";
 
@@ -23,9 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>
-        <CartProvider>
-          {children}
-        </CartProvider>
+        <CartProvider>{children}</CartProvider>
+        <Toaster />
       </body>
     </html>
   );
