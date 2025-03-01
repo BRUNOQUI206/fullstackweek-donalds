50c7d253c25181e9b804a9bbebee7fca9c0b1ee1
Bruno <brunoguilher1@gmail.com>
2025-03-01 10:17:51 -0300

    fix: ensure user is redirected with cpf after order creation

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -57,5 +57,5 @@ export const createOrder = async (input: CreateOrderInput) => {
       resruantId: restaurant.id,
     },
   });
-  redirect(`/${input.slug}/orders`);
+  redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`);
 };
